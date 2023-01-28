2c5a51b3a3d7b879cea236dcc6065296103dfb92
Brent Keller <bikeller104@gmail.com>
2023-01-28 14:43:54 -0700

    created the initial models for the mongoose database

---
+++ models/thought.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280, 
+        },
+        createdAt:
+        {
+            type: Date,
+            default: new Date(),
+            get: formatDate,
+        },
+        username: [
+            {
+                type: String,
+                required: true,
+                ref: 'user',
+            },
+        ],
+        reactions: [
+            {
+                type: String,
+                ref: 'reactions'},
+        ],
+    },
+    {
+        toJSON:{
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtuals('reactionCount').get(function () {
+    return `${this.reactions.Count()}`;
+});
+
+function formatDate(date) {
+    return date.toLocalString();
+}
+
+const Thoughts = model('thoughts', thoughtSchema);
+
+module.exports = Thoughts;
---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const regex = new RegExp(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g`)
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, required: true,
+            unique: true, trim: true
+        },
+        email: {
+            type: String, required: true,
+            unique: true, 
+            validate:
+            {
+                validator: function(value) {
+                    return regex.test(value);
+                }
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts',
+            },
+        ],
+        friends: [
+            {type: Schema.Types.ObjectId,
+            ref: 'user'},
+        ],
+    },
+    {
+        toJSON:{
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtuals('friendCount').get(function () {
+    return `${this.friends.Count()}`;
+});
+
+const User = model('user',userSchema);
+
+module.exports = User;
