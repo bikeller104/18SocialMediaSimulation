edd700b6b0d19861b1fbc96b10888774c9a3feae
Brent Keller <bikeller104@gmail.com>
2023-01-28 18:29:29 -0700

    added thought controller functions to routes

--- routes/api/thoughts/index.js
+++ routes/api/thoughts/index.js
@@ -1,21 +1,21 @@
 const router = require('express').Router();
 
-const {getThoughts, getThought, createThought, deleteThought} = require('./.././.././../controllers/thoughtController');
+const {getThoughts, getThought, createThought, updateThought, deleteThought} = require('./.././.././../controllers/thoughtController');
 
 router.route('/')
 .get(
-
+    getThoughts
     ).post(
-
+        createThought
         );
 
 router.route('/:id')
 .get(
-
+    getThought
     ).put(
-
+        updateThought
         ).delete(
-
+            deleteThought
             );
 
 router.route('/:id/reactions')
